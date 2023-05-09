03922c3714c1f6e610e63e8920635684eda90d72
gustavomfranzin <gusfranzin@gmail.com>
2023-05-08 23:57:49 -0300

    feat: adiciona proxy para comunicação do front com api car query

--- src/api.js
+++ src/api.js
@@ -3,11 +3,14 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes.js';
 import { validateJsonSyntax } from './validators/validationsRoutes.js';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api', createProxyMiddleware({ target: 'https://www.carqueryapi.com', changeOrigin: true }));
 
 app.use(validateJsonSyntax)
 
