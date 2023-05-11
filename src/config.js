b3738172407cba91ffd6bf6aa1a281070517610b
gustavomfranzin <gusfranzin@gmail.com>
2023-05-11 04:18:30 -0300

    feat: criada verificao para caso o token ainda esteja válido retornar o mesmo token, caso o token tenha expirado, gerar um novo e colocado o tempo de expiração em variaveis de ambiente

--- src/config.js
+++ src/config.js
@@ -11,3 +11,7 @@ export const DB_ACCOUNTS = process.env.DB_ACCOUNTS || '';
 export const TOKEN_ACCOUNTS = process.env.TOKEN_ACCOUNTS || '';
 
 export const TOKEN_REFRESH_ACCOUNTS = process.env.TOKEN_REFRESH_ACCOUNTS || '';
+
+export const EXPIRATION_TOKEN_ACCOUNTS = process.env.EXPIRATION_TOKEN_ACCOUNTS || '15m'
+
+export const EXPIRATION_TOKEN_REFRESH_ACCOUNTS = process.env.EXPIRATION_TOKEN_REFRESH_ACCOUNTS || '1d'
--- src/services/users/auth.js
+++ src/services/users/auth.js
@@ -1,5 +1,5 @@
 import { db_accounts } from '../../dbConnect.js';
-import { TOKEN_REFRESH_ACCOUNTS, TOKEN_ACCOUNTS } from '../../config.js'
+import { TOKEN_REFRESH_ACCOUNTS, TOKEN_ACCOUNTS, EXPIRATION_TOKEN_ACCOUNTS, EXPIRATION_TOKEN_REFRESH_ACCOUNTS } from '../../config.js'
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
@@ -23,8 +23,8 @@ export const loginUser = (req, res) => {
             const passwordMatch = bcrypt.compareSync(password, row.password);
 
             if (passwordMatch) {
-                const accessToken = jwt.sign({ userId: row.id }, `${TOKEN_ACCOUNTS}`, { expiresIn: '50s' });
-                const refreshToken = jwt.sign({ userId: row.id }, `${TOKEN_REFRESH_ACCOUNTS}`, { expiresIn: '1d' });
+                const accessToken = jwt.sign({ userId: row.id }, `${TOKEN_ACCOUNTS}`, { expiresIn: `${EXPIRATION_TOKEN_ACCOUNTS}` });
+                const refreshToken = jwt.sign({ userId: row.id }, `${TOKEN_REFRESH_ACCOUNTS}`, { expiresIn: `${EXPIRATION_TOKEN_REFRESH_ACCOUNTS}` });
 
                 db_accounts.run('UPDATE users SET access_token=?, refresh_token=? WHERE id=?', [accessToken, refreshToken, row.id], (err) => {
                     if (err) {
@@ -61,7 +61,7 @@ export const refreshTokens = (req, res) => {
 
         const userId = decoded.userId;
 
-        db_accounts.get('SELECT id FROM users WHERE id=?', [userId], (err, row) => {
+        db_accounts.get('SELECT id, access_token FROM users WHERE id=?', [userId], (err, row) => {
             if (err) {
                 console.error(err.message);
                 return res.status(500).send('Erro ao verificar o usuário');
@@ -71,22 +71,35 @@ export const refreshTokens = (req, res) => {
                 return res.status(401).send('Usuário não encontrado');
             }
 
+            const accessToken = row.access_token;
 
-            const accessToken = jwt.sign({ userId: row.id }, TOKEN_ACCOUNTS, { expiresIn: '1m' });
-
-
-            db_accounts.run('UPDATE users SET access_token=? WHERE id=?', [accessToken, userId], (err) => {
+            // Verifique se o token de acesso atual está expirado
+            jwt.verify(accessToken, TOKEN_ACCOUNTS, (err) => {
                 if (err) {
-                    console.error(err.message);
-                    return res.status(500).send('Erro ao atualizar o token de acesso');
+                    // O token de acesso está expirado, gere um novo token de acesso
+                    const newAccessToken = jwt.sign({ userId: row.id }, TOKEN_ACCOUNTS, { expiresIn: `${EXPIRATION_TOKEN_ACCOUNTS}` });
+
+                    db_accounts.run('UPDATE users SET access_token=? WHERE id=?', [newAccessToken, userId], (err) => {
+                        if (err) {
+                            console.error(err.message);
+                            return res.status(500).send('Erro ao atualizar o token de acesso');
+                        }
+
+                        res.status(200).json({
+                            message: 'Token atualizado',
+                            access_token: newAccessToken
+                        });
+                    });
+                } else {
+                    // O token de acesso atual não está expirado, retorne o mesmo token
+                    res.status(200).json({
+                        message: 'Token dentro da validade',
+                        access_token: accessToken
+                    });
                 }
-
-
-                res.status(200).json({
-                    access_token: accessToken
-                });
             });
         });
     });
 };
+
 
