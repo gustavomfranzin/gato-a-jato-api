baf8e0de5c5c8644af9142ee9b53d328506d0d93
gustavomfranzin <gusfranzin@gmail.com>
2023-05-11 02:08:29 -0300

    Revert "Revert "Revert "feat: criado sistema de autenticação com jwttoken e validação do token no acesso as rotas"""
    This reverts commit 0f131dd3c91b55326ab057e0882bc00e61c6c350.

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { createUserLogin, loginUser } from '../services/users/userRegistration.js';
+import { createUserLogin } from '../services/users/userRegistration.js';
 import { validateUserCreateFields } from '../validators/validationSchema.js';
 
 const router = Router();
 
 router.post('/create-user', validateUserCreateFields, createUserLogin);
-router.post('/login', loginUser)
+
 export default router;
--- src/services/users/userRegistration.js
+++ src/services/users/userRegistration.js
@@ -1,7 +1,5 @@
 import { db_accounts } from '../../dbConnect.js';
-import { TOKE_REFRESH_ACCOUNTS, TOKEN_ACCOUTNS } from '../../config.js'
 import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
 
 
 export const createUserLogin = async (req, res) => {
@@ -15,7 +13,13 @@ export const createUserLogin = async (req, res) => {
     } = req.body;
 
     try {
-        db_accounts.get('SELECT id FROM users WHERE username = ? OR email = ?', [username, email], (err, row) => {
+        const salt = bcrypt.genSaltSync(10);
+
+        const hashedPassword = bcrypt.hashSync(password, salt);
+
+        const sql = 'INSERT INTO users (username, email, password, full_name, date_of_birth, phone_number) VALUES (?, ?, ?, ?, ?, ?)';
+
+        db_accounts.run(sql, [username, email, hashedPassword, full_name, date_of_birth, phone_number], function (err) {
             if (err) {
                 console.error(err.message);
                 res.status(500).send({
@@ -30,99 +34,23 @@ export const createUserLogin = async (req, res) => {
                 });
                 return
             }
-            if (row) {
-                res.status(409).send({
-                    message: 'Usuário ou e-mail já existem',
-                    data: {
-                        username,
-                        email
-                    }
-                })
-                return;
+
+            const response = {
+                message: 'Usuário criado com sucesso',
+                data: {
+                    username,
+                    email,
+                    full_name,
+                    date_of_birth,
+                    phone_number
+                }
             }
 
-            const salt = bcrypt.genSaltSync(10);
+            res.status(201).send(response);
+        });
 
-            const hashedPassword = bcrypt.hashSync(password, salt);
-
-            const sql = 'INSERT INTO users (username, email, password, full_name, date_of_birth, phone_number) VALUES (?, ?, ?, ?, ?, ?)';
-
-            db_accounts.run(sql, [username, email, hashedPassword, full_name, date_of_birth, phone_number], function (err) {
-                if (err) {
-                    console.error(err.message);
-                    res.status(500).send({
-                        message: 'Erro ao criar usuário, formato inválido ou vazio',
-                        data: {
-                            username,
-                            email,
-                            full_name,
-                            date_of_birth,
-                            phone_number
-                        }
-                    });
-                    return
-                }
-
-                const response = {
-                    message: 'Usuário criado com sucesso',
-                    data: {
-                        username,
-                        email,
-                        full_name,
-                        date_of_birth,
-                        phone_number
-                    }
-                }
-                res.status(201).send(response);
-            });
-        })
     } catch (err) {
         console.error(err);
         res.status(500).send('Server Error')
     }
-}
-
-export const loginUser = (req, res) => {
-    const { username, password } = req.body;
-    console.log({ username, password });
-
-    db_accounts.get('SELECT id, username, password FROM users WHERE username=?', [username], (err, row) => {
-        if (err) {
-            console.error(err.message);
-            res.status(500).send({
-                message: 'Erro ao ler o usuário',
-                data: {
-                    username
-                }
-            });
-            return
-        }
-        if (!row) {
-            console.log(row)
-            res.status(401).send('Usuário não encontrado');
-        } else {
-            const passwordMatch = bcrypt.compareSync(password, row.password);
-            console.log(passwordMatch);
-            if (passwordMatch) {
-                const accessToken = jwt.sign({ userId: row.id }, `${TOKEN_ACCOUTNS}`, { expiresIn: '30m' });
-                const refreshToken = jwt.sign({ userId: row.id }, `${TOKE_REFRESH_ACCOUNTS}`, { expiresIn: '7d' });
-
-                db_accounts.run('UPDATE users SET access_token=?, refresh_token=? WHERE id=?', [accessToken, refreshToken, row.id], (err) => {
-                    if (err) {
-                        console.error(err.message);
-                        res.status(500).send('Erro ao salvar os tokens');
-                    } else {
-                        res.status(200).json({
-                            message: 'Usuário autenticado com sucesso',
-                            access_token: accessToken,
-                            refresh_token: refreshToken
-                        });
-                    }
-                });
-
-            } else {
-                res.status(401).send('Usuário ou senha inválidos');
-            }
-        }
-    })
 }
