8c80804d7b168edbbc10c5ee0228f31b38ebd857
gustavomfranzin <gusfranzin@gmail.com>
2023-05-11 03:22:13 -0300

    add: adiciona funcao para criar novo token e corrige variavel de ambiente

--- src/config.js
+++ src/config.js
@@ -10,4 +10,4 @@ export const DB_ACCOUNTS = process.env.DB_ACCOUNTS || '';
 
 export const TOKEN_ACCOUNTS = process.env.TOKEN_ACCOUNTS || '';
 
-export const TOKE_REFRESH_ACCOUNTS= process.env.TOKE_REFRESH_ACCOUNTS || '';
+export const TOKEN_REFRESH_ACCOUNTS = process.env.TOKEN_REFRESH_ACCOUNTS || '';
--- src/services/users/auth.js
+++ src/services/users/auth.js
@@ -1,5 +1,5 @@
 import { db_accounts } from '../../dbConnect.js';
-import { TOKE_REFRESH_ACCOUNTS, TOKEN_ACCOUNTS } from '../../config.js'
+import { TOKEN_REFRESH_ACCOUNTS, TOKEN_ACCOUNTS } from '../../config.js'
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
@@ -24,7 +24,7 @@ export const loginUser = (req, res) => {
             console.log(passwordMatch);
             if (passwordMatch) {
                 const accessToken = jwt.sign({ userId: row.id }, `${TOKEN_ACCOUNTS}`, { expiresIn: '30m' });
-                const refreshToken = jwt.sign({ userId: row.id }, `${TOKE_REFRESH_ACCOUNTS}`, { expiresIn: '7d' });
+                const refreshToken = jwt.sign({ userId: row.id }, `${TOKEN_REFRESH_ACCOUNTS}`, { expiresIn: '7d' });
 
                 db_accounts.run('UPDATE users SET access_token=?, refresh_token=? WHERE id=?', [accessToken, refreshToken, row.id], (err) => {
                     if (err) {
@@ -45,3 +45,48 @@ export const loginUser = (req, res) => {
         }
     })
 }
+
+export const refreshTokens = (req, res) => {
+    const refreshToken = req.body.refresh_token;
+
+    if (!refreshToken) {
+        return res.status(400).send('Token de atualização não fornecido');
+    }
+
+    jwt.verify(refreshToken, TOKEN_REFRESH_ACCOUNTS, (err, decoded) => {
+        if (err) {
+            console.error(err);
+            return res.status(401).send('Token de atualização inválido');
+        }
+
+        const userId = decoded.userId;
+
+        db_accounts.get('SELECT id FROM users WHERE id=?', [userId], (err, row) => {
+            if (err) {
+                console.error(err.message);
+                return res.status(500).send('Erro ao verificar o usuário');
+            }
+
+            if (!row) {
+                return res.status(401).send('Usuário não encontrado');
+            }
+
+
+            const accessToken = jwt.sign({ userId: row.id }, TOKEN_ACCOUNTS, { expiresIn: '30m' });
+
+
+            db_accounts.run('UPDATE users SET access_token=? WHERE id=?', [accessToken, userId], (err) => {
+                if (err) {
+                    console.error(err.message);
+                    return res.status(500).send('Erro ao atualizar o token de acesso');
+                }
+
+
+                res.status(200).json({
+                    access_token: accessToken
+                });
+            });
+        });
+    });
+};
+
