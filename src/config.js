b463854d69b75fbfd97943c281c4c7005948f236
gustavomfranzin <gusfranzin@gmail.com>
2023-05-11 02:02:05 -0300

    feat: criado sistema de autenticação com jwttoken e validação do token no acesso as rotas

--- src/config.js
+++ src/config.js
@@ -7,3 +7,7 @@ export const PORT = process.env.PORT || '';
 export const DB_HOST = process.env.DB_HOST || '';
 
 export const DB_ACCOUNTS = process.env.DB_ACCOUNTS || '';
+
+export const TOKEN_ACCOUTNS = process.env.TOKEN_ACCOUTNS || '';
+
+export const TOKE_REFRESH_ACCOUNTS= process.env.TOKE_REFRESH_ACCOUNTS || '';
--- src/routes/carRegistrationRoutes.js
+++ src/routes/carRegistrationRoutes.js
@@ -1,14 +1,16 @@
 import { Router } from 'express';
 import { createEntryCustomer, readCustomerCreated, updateCustomerCreated, deleteCustomerCreated } from '../services/carRegistration.js';
 import { validateFields } from '../validators/validationSchema.js';
+import { verifyToken } from '../validators/validationToken.js'
+
 const router = Router();
 
-router.post('/new-entry', validateFields, createEntryCustomer);
+router.post('/new-entry', verifyToken, validateFields, createEntryCustomer);
 
-router.get('/list-entries', readCustomerCreated)
+router.get('/list-entries', verifyToken, readCustomerCreated)
 
-router.put('/update-entry/:id', validateFields, updateCustomerCreated);
+router.put('/update-entry/:id', verifyToken, validateFields, updateCustomerCreated);
 
-router.delete('/delete-entry/:id', deleteCustomerCreated)
+router.delete('/delete-entry/:id', verifyToken, deleteCustomerCreated)
 
 export default router;
