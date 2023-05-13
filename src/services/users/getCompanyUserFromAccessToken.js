455940aa674b2d897cd6312ef6261a18fe085eff
gustavomfranzin <gusfranzin@gmail.com>
2023-05-12 21:17:19 -0300

    refactor: ajustes das variaveis de ambiente e remoção de comentários

--- src/services/users/getCompanyUserFromAccessToken.js
+++ src/services/users/getCompanyUserFromAccessToken.js
@@ -2,9 +2,8 @@ import { TOKEN_ACCOUNTS } from '../../config.js';
 import jwt from 'jsonwebtoken';
 
 export const getCompanyUserFromAccessToken = (access_token) => {
-    const secretKey = TOKEN_ACCOUNTS;
     try {
-        const decoded = jwt.verify(access_token, secretKey);
+        const decoded = jwt.verify(access_token, TOKEN_ACCOUNTS);
         const codCompany = decoded.codCompany;
         return codCompany;
     } catch (err) {
